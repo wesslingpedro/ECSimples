96651f1f27a3286f36f1af92a75670aeb7734b16
wesslingpedro <wesslingpedro@gmail.com>
2024-05-15 19:42:48 -0300

    Primeiro commit

---
+++ EstruturaCondicional/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace testes_ec{
+    class Program{
+        static void Main(string[] args) {
+
+            int x = 5;
+            Console.WriteLine("Bom dia");
+
+            if (x > 6)
+            {
+                // estrutura condicional simples
+            
+
+            Console.WriteLine("Boa tarde");
+        }
+            Console.WriteLine("Boa noite");
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
